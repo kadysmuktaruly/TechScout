8c60c4a2af46b097fe15f26be3174db782816f42
kadysmuktaruly <kadysmuktar06@gmail.com>
2025-08-24 18:51:02 +0500

    styled it little more

--- jobs/views.py
+++ jobs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Job
 from .forms import JobForm
+from django.shortcuts import render, get_object_or_404
+
 
 
 def home(request):
@@ -31,7 +33,8 @@ def job_list(request):
     return render(request, "jobs/job_list.html", {"jobs": jobs})
 
 def job_detail(request, job_id):
-    return HttpResponse(f"Job detail {job_id}")
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/job_detail.html', {'job': job})
 
 def job_create(request):
     if request.method == "POST":
